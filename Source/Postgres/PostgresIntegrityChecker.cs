e8bf91effb4ac2de4cbd148a98bec83c04624c33
jachman <3646345+BitKFu@users.noreply.github.com>
2010-08-15 20:19:50 +0000

    Fix Postgres Integrity Check for Memo Types

--- Source/Postgres/PostgresIntegrityChecker.cs
+++ Source/Postgres/PostgresIntegrityChecker.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using AdFactum.Data.Internal;
+using AdFactum.Data.Repository;
 
 namespace AdFactum.Data.Postgres
 {
@@ -15,5 +16,40 @@ namespace AdFactum.Data.Postgres
             : base(persister, typeMapper, databaseSchema)
         {
         }
+
+        /// <summary>
+        /// Gets the integrity of an object type.
+        /// </summary>
+        protected override IEnumerable<IntegrityInfo> CheckIntegrity(IntegrityInfo info)
+        {
+            var infoCollection = base.CheckIntegrity(info);
+            info = infoCollection.First();
+
+            /*
+             * Remove all Required Failures for Microsoft Access, because the 
+             * ADO .NET Provider does not read the AllowDBNull Value in Schema correctly
+             */
+            info.MismatchedFields.RemoveAll(field => field.RequiredFailure);
+            info.MismatchedFields.RemoveAll(field => field.UniqueFailure);
+
+            return infoCollection;
+        }
+
+        protected override int CalculateSize(int size)
+        {
+            if (size == -1)
+                size = int.MaxValue;
+
+            return size;
+        }
+
+        protected override int CalculateUnicodeSize(int size)
+        {
+            if (size == -1)
+                size = int.MaxValue/2;
+
+            return size;
+        }
+
     }
 }
