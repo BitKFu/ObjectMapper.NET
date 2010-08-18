783cb804e5ee464255a51e197e670070b9b578c2
jachman <3646345+BitKFu@users.noreply.github.com>
2010-08-18 21:05:12 +0000

    Add missing Namespace

--- Source/Linq/Translation/OrderByRewriter.cs
+++ Source/Linq/Translation/OrderByRewriter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using AdFactum.Data.Linq.Expressions;
 using AdFactum.Data.Queries;
