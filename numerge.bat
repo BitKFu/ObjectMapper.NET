6bf1eed766584f306eb47983802f738265a94810
jachman <3646345+BitKFu@users.noreply.github.com>
2021-04-27 12:10:56 +0000

    Fix creating nuget packages

--- numerge.bat
+++ numerge.bat
@@ -3,10 +3,33 @@ if %1s==s (
  goto ende
 )
 
-NupkgMerge -p .\ObjectMapper.NET_net472.%1.nupkg -s .\ObjectMapper.NET_Standard20.%1.nupkg -o tmp.nupkg
-NupkgMerge.exe -p .\tmp.nupkg -s .\ObjectMapper.NET_Standard21.%1.nupkg -o ObjectMapper.NET.%1.nupkg
+cd output
+
+cd release
+NupkgMerge -p .\ObjectMapper.NET.%1.nupkg -s .\ObjectMapper.NET_Standard20.%1.nupkg -o tmp.nupkg
+NupkgMerge.exe -p .\tmp.nupkg -s .\ObjectMapper.NET_net472.%1.nupkg -o ..\ObjectMapper.NET.%1.nupkg
 del tmp.nupkg
+del ObjectMapper.NET.%1.nupkg
 del ObjectMapper.NET_net472.%1.nupkg
 del ObjectMapper.NET_Standard20.%1.nupkg
-del ObjectMapper.NET_Standard21.%1.nupkg
+cd..
+
+cd release.oracle
+NupkgMerge -p .\ObjectMapper.NET_Oracle.%1.nupkg -s .\ObjectMapper.NET_Oracle_Standard20.%1.nupkg -o tmp.nupkg
+NupkgMerge.exe -p .\tmp.nupkg -s .\ObjectMapper.NET_Oracle_net472.%1.nupkg -o ..\ObjectMapper.NET_Oracle.%1.nupkg
+del tmp.nupkg
+del ObjectMapper.NET_Oralce.%1.nupkg
+del ObjectMapper.NET_Oralce_net472.%1.nupkg
+del ObjectMapper.NET_Oralce_Standard20.%1.nupkg
+cd..
+
+cd release.postgres
+NupkgMerge -p .\ObjectMapper.NET_Postgres.%1.nupkg -s .\ObjectMapper.NET_Postgres_Standard20.%1.nupkg -o tmp.nupkg
+NupkgMerge.exe -p .\tmp.nupkg -s .\ObjectMapper.NET_Postgres_net472.%1.nupkg -o ..\ObjectMapper.NET_Postgres.%1.nupkg
+del tmp.nupkg
+del ObjectMapper.NET_Oralce.%1.nupkg
+del ObjectMapper.NET_Oralce_net472.%1.nupkg
+del ObjectMapper.NET_Oralce_Standard20.%1.nupkg
+cd..
+
 :ende
